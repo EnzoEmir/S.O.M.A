461cbdf77f53f09aa6e056dcd7cee215a7c70878
EnzoEmir <164296530+EnzoEmir@users.noreply.github.com>
2025-08-10 12:15:47 -0300

    Testando colorir os dias

--- SOMA/Agenda/ui.py
+++ SOMA/Agenda/ui.py
@@ -1,6 +1,6 @@
-from PySide6.QtWidgets import QWidget, QPushButton, QVBoxLayout
-from PySide6.QtCore import Qt
-
+from PySide6.QtWidgets import QWidget, QPushButton, QVBoxLayout, QCalendarWidget
+from PySide6.QtGui import QColor, QTextCharFormat
+from PySide6.QtCore import QDate
 from SOMA.Agenda import controller
 
 class AgendaWindow(QWidget):
@@ -9,7 +9,18 @@ class AgendaWindow(QWidget):
         self.setWindowTitle("Agenda")
         self.resize(360, 420)
 
+        self.calendario = QCalendarWidget()
+    
+        teste = QDate(2024, 1, 1)
+        hoje = QDate.currentDate()
+        formato_dia_especial = QTextCharFormat() 
+        cor_de_fundo = QColor("#a4bcdd")
+        formato_dia_especial.setBackground(cor_de_fundo)
+        self.calendario.setDateTextFormat(hoje, formato_dia_especial)
+        self.calendario.setDateTextFormat(teste, formato_dia_especial)
+
         layout = QVBoxLayout()
+        layout.addWidget(self.calendario)
         self.setLayout(layout)
 
         btn_Voltar = QPushButton("❌ Voltar")
