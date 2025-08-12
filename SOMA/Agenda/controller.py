316def83129bbd084adcf9fbefc124928ac556b3
EnzoEmir <164296530+EnzoEmir@users.noreply.github.com>
2025-08-12 12:31:49 -0300

    Reorganizando estrutura

--- SOMA/Agenda/controller.py
+++ SOMA/Agenda/controller.py
@@ -1,3 +1,68 @@
+import json
+from PySide6.QtGui import QColor, QTextCharFormat, QFont
+from PySide6.QtCore import QDate
+
+class Controller:
+    def __init__(self, view):
+        self.view = view
+        
+        self.tarefas = []
+        self.filtro_atual = "all"
+
+        self.formato_dias_importantes = QTextCharFormat()
+        self.formato_dias_importantes.setBackground(QColor("#a4bcdd"))
+        self.formato_dias_importantes.setFontWeight(QFont.Bold)
+        
+        self.carregar_tarefas_json()
+        self.atualizar_grifados()
+
+    def carregar_tarefas_json(self):
+        try:
+            with open("SOMA/minhas_datas.json", "r", encoding="utf-8") as f:
+                self.tarefas = json.load(f)
+        except FileNotFoundError:
+            self.tarefas = []
+            print("AVISO: Arquivo 'minhas_datas.json' não encontrado.")
+    
+    def atualizar_grifados(self):
+        tipo_filtro = self.filtro_atual
+        self.view.calendario.setDateTextFormat(QDate(), QTextCharFormat()) 
+        self.aplicar_formatacao(tipo_filtro)
+
+    def atualizar_filtro(self, tipo_filtro):
+        self.filtro_atual = tipo_filtro
+        self.atualizar_grifados()
+
+    def aplicar_formatacao(self, tipo_filtro="all"):
+        ano = self.view.calendario.yearShown()
+        mes = self.view.calendario.monthShown()
+
+        for tarefa in self.tarefas:
+            tipo_tarefa = tarefa["type"]
+
+            if tipo_filtro != "all" and tipo_tarefa != tipo_filtro:
+                continue
+
+            data_original = QDate.fromString(tarefa["date"], "dd-MM-yyyy")
+
+            if tipo_tarefa == "single":
+                if data_original.year() == ano and data_original.month() == mes:
+                    self.view.calendario.setDateTextFormat(data_original, self.formato_dias_importantes)
+
+            elif tipo_tarefa == "daily":
+                dias_no_mes = QDate(ano, mes, 1).daysInMonth()
+                for dia in range(1, dias_no_mes + 1):
+                    self.view.calendario.setDateTextFormat(QDate(ano, mes, dia), self.formato_dias_importantes)
+
+            elif tipo_tarefa == "weekly":
+                dia_semana = data_original.dayOfWeek()
+                dias_no_mes = QDate(ano, mes, 1).daysInMonth()
+                for dia in range(1, dias_no_mes + 1):
+                    data_atual = QDate(ano, mes, dia)
+                    if data_atual.dayOfWeek() == dia_semana:
+                        self.view.calendario.setDateTextFormat(data_atual, self.formato_dias_importantes)
+
+
 def voltar_main(current_window):
     from SOMA.inicial.ui import MainWindow
     
--- SOMA/Agenda/ui.py
+++ SOMA/Agenda/ui.py
@@ -1,8 +1,5 @@
 from PySide6.QtWidgets import QWidget, QPushButton, QVBoxLayout, QCalendarWidget
-from PySide6.QtGui import QColor, QTextCharFormat, QFont
-from PySide6.QtCore import QDate
 from SOMA.agenda import controller
-import json
 
 class AgendaWindow(QWidget):
     def __init__(self):
@@ -12,105 +9,30 @@ class AgendaWindow(QWidget):
 
         self.calendario = QCalendarWidget()
 
-        self.formato_dias_importantes = QTextCharFormat() 
-        cor_de_fundo = QColor("#a4bcdd")
-        self.formato_dias_importantes.setBackground(cor_de_fundo)
-        self.formato_dias_importantes.setFontWeight(QFont.Bold)
+        self.btn_todos = QPushButton("Ver Todos")
+        self.btn_diarios = QPushButton("Diários")
+        self.btn_semanais = QPushButton("Semanais")
+        self.btn_unicos = QPushButton("Evento Único")
+        self.btn_voltar = QPushButton("❌ Voltar")
 
-        try: 
-            with open("SOMA\minhas_datas.json", "r") as f:
-                lista_de_tarefas = json.load(f)
-        except FileNotFoundError: 
-            lista_de_tarefas = [] 
-            print("AVISO: Arquivo 'minhas_datas.json' não encontrado.")
-
+        layout = QVBoxLayout(self) 
         
-        for tarefa in lista_de_tarefas:             
-            q_date = QDate.fromString(tarefa["date"], "dd-MM-yyyy")
-
-            self.calendario.setDateTextFormat(q_date, self.formato_dias_importantes)
-
-        layout = QVBoxLayout()
-        
-
-
-        btn_todos = QPushButton("Ver Todos")
-        btn_diarios = QPushButton("Diários")
-        btn_semanais = QPushButton("Semanais")
-        btn_unicos = QPushButton("Evento Único")
-        btn_voltar = QPushButton("❌ Voltar")
-
-
         layout.addWidget(self.calendario)
+        layout.addWidget(self.btn_todos)
+        layout.addWidget(self.btn_diarios)
+        layout.addWidget(self.btn_semanais)
+        layout.addWidget(self.btn_unicos)
+        layout.addWidget(self.btn_voltar)
+        
+        self.controller = controller.Controller(self)
 
-        layout.addWidget(btn_todos)
-        layout.addWidget(btn_diarios)
-        layout.addWidget(btn_semanais)
-        layout.addWidget(btn_unicos)
-        layout.addWidget(btn_voltar)
+        self.btn_todos.clicked.connect(lambda: self.controller.atualizar_filtro("all"))
+        self.btn_diarios.clicked.connect(lambda: self.controller.atualizar_filtro("daily"))
+        self.btn_semanais.clicked.connect(lambda: self.controller.atualizar_filtro("weekly"))
+        self.btn_unicos.clicked.connect(lambda: self.controller.atualizar_filtro("single"))
+        
+        self.btn_voltar.clicked.connect(lambda: controller.voltar_main(self))
 
-        self.setLayout(layout)
-
-        self.carregar_tarefas_json()
-
-        self.filtro_atual = "all"
-
-        self.atualizar_grifados() 
-
-        btn_todos.clicked.connect(lambda: self.atualizar_filtro("all"))
-        btn_diarios.clicked.connect(lambda: self.atualizar_filtro("daily"))
-        btn_semanais.clicked.connect(lambda: self.atualizar_filtro("weekly"))
-        btn_unicos.clicked.connect(lambda: self.atualizar_filtro("single"))
-        btn_voltar.clicked.connect(lambda: controller.voltar_main(self))
-
-        self.calendario.currentPageChanged.connect(lambda: self.atualizar_grifados(self.filtro_atual))
-
-
-    def carregar_tarefas_json(self):
-        try:
-            with open("SOMA\minhas_datas.json", "r", encoding="utf-8") as f:
-                self.tarefas = json.load(f)
-        except FileNotFoundError:
-            self.tarefas = []
-            print("AVISO: Arquivo 'minhas_datas.json' não encontrado.")
-    
-    def atualizar_grifados(self, tipo_filtro="all"): 
-        self.calendario.setDateTextFormat(QDate(), QTextCharFormat())
-        self.aplicar_formatacao(tipo_filtro)
-
-    def atualizar_filtro(self, tipo_filtro):
-        self.filtro_atual = tipo_filtro
-        self.atualizar_grifados(tipo_filtro)
-
-    def aplicar_formatacao(self, tipo_filtro="all"):
-        ano = self.calendario.yearShown()
-        mes = self.calendario.monthShown()
-
-        for tarefas in self.tarefas:
-            tipo_tarefa = tarefas["type"]
-
-            if tipo_filtro != "all" and tipo_tarefa != tipo_filtro:
-                continue
-
-            data_original = QDate.fromString(tarefas["date"], "dd-MM-yyyy")
-
-            if tipo_tarefa == "single":
-                if data_original.year() == ano and data_original.month() == mes:
-                    self.calendario.setDateTextFormat(data_original, self.formato_dias_importantes)
-
-            elif tipo_tarefa == "daily":
-                dias_no_mes = QDate(ano, mes, 1).daysInMonth()
-                for dia in range(1, dias_no_mes + 1):
-                    self.calendario.setDateTextFormat(QDate(ano, mes, dia), self.formato_dias_importantes)
-
-            elif tipo_tarefa == "weekly":
-                dia_semana = data_original.dayOfWeek()
-                dias_no_mes = QDate(ano, mes, 1).daysInMonth()
-                for dia in range(1, dias_no_mes + 1):
-                    data_atual = QDate(ano, mes, dia)
-                    if data_atual.dayOfWeek() == dia_semana:
-                        self.calendario.setDateTextFormat(data_atual, self.formato_dias_importantes)
-
-    
+        self.calendario.currentPageChanged.connect(self.controller.atualizar_grifados)
 
         self.main = None
